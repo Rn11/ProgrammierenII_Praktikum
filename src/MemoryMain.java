b2e81267fc30609ad504705eb1cef5f2147a6804
Max <Rn11@DESKTOP-41JF6NQ>
2017-05-08 23:48:38 +0200

    Fenster wurde erstellt, buttons hinzugefuegt

--- src/MemoryMain.java
+++ src/MemoryMain.java
@@ -1,9 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class MemoryMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame fenster = new JFrame("Memory");
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		// fenstergroesse 600x600
+		fenster.setSize(600, 600);
+		fenster.setVisible(true);
+
+		// 2x2 Raster erstellen
+		fenster.setLayout(new GridLayout(2, 2));
+		fenster.getContentPane().add(new JButton("1"));
+		fenster.getContentPane().add(new JButton("2"));
+		fenster.getContentPane().add(new JButton("3"));
+		fenster.getContentPane().add(new JButton("4"));
 	}
-
 }
