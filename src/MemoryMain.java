1d1253a21a25a0802ccfb793a03448725e457e39
Max <max.lukas-nuelle@stud.hshl.de>
2017-05-09 03:18:57 +0200

    GUI in Methode ausgelagert, listener hinzugefuegt

--- src/MemoryMain.java
+++ src/MemoryMain.java
@@ -1,21 +1,60 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class MemoryMain {
+	static JFrame screen;
 
 	public static void main(String[] args) {
-		JFrame fenster = new JFrame("Memory");
-		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		prepareGUI();
+	}
 
-		// fenstergroesse 600x600
-		fenster.setSize(600, 600);
-		fenster.setVisible(true);
+	static void prepareGUI() {
+		screen = new JFrame("Memory");
+		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// screengroesse 600x600
+		screen.setSize(600, 600);
+		screen.setVisible(true);
 
 		// 2x2 Raster erstellen
-		fenster.setLayout(new GridLayout(2, 2));
-		fenster.getContentPane().add(new JButton("1"));
-		fenster.getContentPane().add(new JButton("2"));
-		fenster.getContentPane().add(new JButton("3"));
-		fenster.getContentPane().add(new JButton("4"));
+		screen.setLayout(new GridLayout(2, 2));
+
+		// buttons erstellen
+		JButton button1 = new JButton("1");
+		JButton button2 = new JButton("2");
+		JButton button3 = new JButton("3");
+		JButton button4 = new JButton("4");
+
+		screen.getContentPane().add(button1);
+		screen.getContentPane().add(button2);
+		screen.getContentPane().add(button3);
+		screen.getContentPane().add(button4);
+
+		// event listener
+		button1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// aendere hintergrundfarbe
+				button1.setBackground(Color.green);
+			}
+		});
+
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+
+		button3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+
+		button4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
 	}
 }
